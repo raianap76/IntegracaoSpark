050ea44193f16fc9a7be7565cebf1fde414d8ddf
raianap76 <raianap76@hotmail.com>
2022-04-26 22:24:29 -0300

    first commit

---
+++ src/main/java/app/Aplicacao.java
@@ -0,0 +1,34 @@
+package app;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
+
+import service.ProdutoService;
+
+public class Aplicacao {
+
+	private static ProdutoService produtoService = new ProdutoService();
+	
+    public static void main(String[] args) {
+        port(6789);
+        
+        staticFiles.location("/public");
+        
+        post("/produto/insert", (request, response) -> produtoService.insert(request, response));
+
+        get("/produto/:id", (request, response) -> produtoService.get(request, response));
+        
+        get("/produto/list/:orderby", (request, response) -> produtoService.getAll(request, response));
+
+        get("/produto/update/:id", (request, response) -> produtoService.getToUpdate(request, response));
+        
+        post("/produto/update/:id", (request, response) -> produtoService.update(request, response));
+           
+        get("/produto/delete/:id", (request, response) -> produtoService.delete(request, response));
+
+             
+    }
+
+}
